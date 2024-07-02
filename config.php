6d83cb048a2d8a76e5f5d3ab1ee4575df7d407fa
justin <justin20745413@gmail.com>
2024-07-02 14:17:30 +0800

    修改php

--- config.php
+++ config.php
@@ -1,6 +1,17 @@
 <?php
 //--------------------開發階段
-header("Access-Control-Allow-Origin:http://localhost:5174");
+$allowed_origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
--- knowledgeUpdate.php
+++ knowledgeUpdate.php
@@ -4,14 +4,14 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 try {
     require_once("config.php");
-    $sql = "UPDATE knowledge (K_TITLE, K_CONTENT, K_FROM, K_URL, K_DATE) 
-    VALUES (:K_TITLE, :K_CONTENT, :K_FROM, :K_URL, :K_DATE)";
+    $sql = "UPDATE knowledge SET K_TITLE = :K_TITLE, K_CONTENT = :K_CONTENT, K_FROM = :K_FROM, K_URL = :K_URL, K_DATE = :K_DATE WHERE K_ID = :K_ID";
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(":K_TITLE", $data["K_TITLE"]);
     $stmt->bindValue(":K_CONTENT", $data["K_CONTENT"]);
     $stmt->bindValue(":K_FROM", $data["K_FROM"]);
     $stmt->bindValue(":K_URL", $data["K_URL"]);
     $stmt->bindValue(":K_DATE", $data["K_DATE"]);
+    $stmt->bindValue(":K_ID", $data["K_ID"]); 
     $stmt->execute();
 
     echo json_encode(["error" => false, "msg" => "修改成功"]);
