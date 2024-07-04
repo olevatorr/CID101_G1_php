3ddb024819beacb1c39c01da40dc7b27f297d765
olevatorr <otis.coding@gmail.com>
2024-07-05 02:57:11 +0800

    add/ 新增管理員修改/ 刪除/ 改狀態/ 抓資料

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    require_once("config.php"); // 引入資料庫配置文件
+
+    $sql = "select * from ADMIN"; // 準備 SQL 查詢語句，從資料庫中選擇所有知識數據
+    $admin = $pdo->query($sql); // 執行 SQL 查詢調用pdo裡的函式
+    $prodRows = $admin->fetchAll(PDO::FETCH_ASSOC); // 獲取所有查詢結果行，並以關聯數組的形式返回
+
+    $result = ["error" => false, "msg" => "", "admin" => $prodRows]; // 準備成功的 JSON 響應數據
+} catch (PDOException $e) {
+    $result = ["error" => true, "msg" => $e->getMessage()]; // 捕獲 PDO 異常，並準備錯誤的 JSON 響應數據
+}
+
+echo json_encode($result, JSON_NUMERIC_CHECK); // 將 PHP 數組轉換為 JSON 格式並輸出
+
+?>
---
+++ adminDelete.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/json');
+try {
+    // 引入資料庫連接配置文件
+        require_once("config.php");
+    
+        // 定義 SQL 刪除語句，用於刪除 `knowledge` 表中指定 `K_ID` 的記錄
+        $sql = "delete from ADMIN where AD_ID = ?";
+    
+        // 使用 PDO 對象準備 SQL 語句
+        $admin = $pdo->prepare($sql);
+    
+        // 綁定 GET 請求中的 `K_ID` 參數到 SQL 語句中的第一個問號佔位符
+        $admin->bindValue(1, $_GET["AD_ID"]);
+    
+        // 執行 SQL 語句
+        $admin->execute();
+    
+        // 獲取受影響的行數
+        $affectedCount = $admin->rowCount();
+    
+        // 創建結果數組，包含操作是否成功的標誌和影響的行數
+        $result = ["error" => false, "msg" => "成功的影響{$affectedCount}筆"];
+    } catch (PDOException $e) {
+        // 如果發生例外，創建結果數組，包含錯誤標誌和錯誤信息
+        $result = ["error" => true, "msg" => $e->getMessage()];
+    }
+    
+    // 將結果數組編碼為 JSON 格式並輸出
+    echo json_encode($result, JSON_NUMERIC_CHECK);
+?>
