5455e9297d4657be7a5ee8e789fed0a542167ac8
Rahaydn <lexea53@gmail.com>
2024-07-12 02:23:27 +0800

    刪除解說

--- updateSuspension.php
+++ updateSuspension.php
@@ -3,15 +3,8 @@ header('Content-Type: application/json');
 
 require_once("config.php");
 
-<<<<<<< HEAD
 try {
     $data = json_decode(file_get_contents('php://input'), true);
-=======
-
-try {
-    $data = json_decode(file_get_contents('php://input'), true);
-
->>>>>>> 93863d730fd83529042ced6b7edd14bf816c09f1
 
     $pdo->beginTransaction();
     $sql = "UPDATE USER SET U_STATUS = :U_STATUS WHERE U_ID = :U_ID";
