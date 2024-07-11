1131f4b006aa7ebbde2bfd54ed5cd8d6d56185c2
olevatorr <otis.coding@gmail.com>
2024-07-12 01:32:52 +0800

    fixed

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 //--------------------開發階段
 $allowed_origins = [
-    "*"
+    "http://localhost:5173",//前台或後台網址
+    "http://localhost:5174",
+    "http://localhost:5175",
 ];
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
@@ -13,16 +15,16 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
-}	
+}
 //---
-// $dbname = "bluealert";
-// $user = "root";
-// $password = "";
+$dbname = "bluealert";
+$user = "root";
+$password = "";
 
 //--------------------prod階段
-$dbname = "tibamefe_cid101g1";
-$user = "tibamefe_since2021";
-$password = "vwRBSb.j&K#E";
+// $dbname = "tibamefe_cid101g1";
+// $user = "tibamefe_since2021";
+// $password = "vwRBSb.j&K#E";
 
 $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
 
